c9c24997eb1c6c0c3b0b0049a0079a02de1f1404
Md Rashedul Karim <therashedul@gmail.com>
2024-08-08 15:18:05 +0600

    first commit

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,65 @@
+
+
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Example Component</div>
+
+                    <div class="card-body">
+                         <h2>Counter: {{ count }}</h2>
+    <button @click="increment">Increment</button>
+                        I'm an example component.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      count: 0
+    };
+  },
+  methods: {
+    increment() {
+      this.count++;
+    }
+  },
+    mounted() {
+    // methods can be called in lifecycle hooks, or other methods!
+    this.increment()
+  },
+  beforeCreate() {
+    console.log('beforeCreate');
+  },
+  created() {
+    console.log('created');
+  },
+  beforeMount() {
+    console.log('beforeMount');
+  },
+  mounted() {
+    console.log('mounted');
+  },
+  beforeUpdate() {
+    console.log('beforeUpdate');
+  },
+  updated() {
+    console.log('updated');
+  },
+  beforeDestroy() {
+    console.log('beforeDestroy');
+  },
+  destroyed() {
+    console.log('destroyed');
+  }
+};
+</script>
+
+
+
